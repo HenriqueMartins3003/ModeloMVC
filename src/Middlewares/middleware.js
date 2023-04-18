0ca3cf5f3e29190f7933d642532efe51e4e20acd
Henrique de França Martins <hique_martins@hotmail.com>
2023-04-17 21:41:46 -0300

    Modelo MVC

--- src/Middlewares/middleware.js
+++ src/Middlewares/middleware.js
@@ -1,8 +1,19 @@
+//Meu middleWare Global
 exports.middlewareGlobal = (req,res,next)=>{
+    res.locals.umaVariavelLocal = 'Está é uma variavel local'
     next()
 }
 
-exports.outroMiddleware = (req,res,next) => {
+//Meu MiddleWare de erro do CSRF
+exports.checkCsrfError = (err,req,res,next) => {
+    if(err && 'EBADCSRFTOKEN' === err.code) {
+        console.log(err.code)
+        return res.render('404')
+    }
+    next()       
+}
+//Criador de Token do CSRF
+exports.csrfCriaToken = (req,res,next) => {
+    res.locals.csrfToken = req.csrfToken()
     next()
 }
-
